cf0c00e65923b02412c10f9f6a9281d1bac230a8
sinderpl <sinderpl@gmail.com>
2017-02-01 17:42:48 +0000

    Ionic and angular 2 example used instead

---
+++ .tmp/pages/tabs/tabs.js
@@ -0,0 +1,29 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+import { Component } from '@angular/core';
+import { PingPage } from '../ping/ping';
+import { ProfilePage } from '../profile/profile';
+import { AuthService } from '../../services/auth/auth.service';
+export var TabsPage = (function () {
+    function TabsPage(auth) {
+        this.auth = auth;
+        // this tells the tabs component which Pages
+        // should be each tab's root Page
+        this.tab1Root = PingPage;
+        this.tab2Root = ProfilePage;
+    }
+    TabsPage = __decorate([
+        Component({template:/*ion-inline-start:"/home/pancakemutiny/Desktop/collegeProjects/bankingApplication/src/pages/tabs/tabs.html"*/'<ion-tabs>\n  <ion-tab [root]="tab1Root" tabTitle="Ping" tabIcon="pulse"></ion-tab>\n  <ion-tab [root]="tab2Root" tabTitle="Profile" tabIcon="person"></ion-tab>\n</ion-tabs>\n'/*ion-inline-end:"/home/pancakemutiny/Desktop/collegeProjects/bankingApplication/src/pages/tabs/tabs.html"*/
+        }), 
+        __metadata('design:paramtypes', [AuthService])
+    ], TabsPage);
+    return TabsPage;
+}());
+//# sourceMappingURL=tabs.js.map
--- www/auth0.variables.js
+++
@@ -1,3 +0,0 @@
-var AUTH0_CLIENT_ID='nzVv3groJ40GF3tk956Qj4TF0E0ubvh7';
-var AUTH0_CALLBACK_URL= location.href;
-var AUTH0_DOMAIN='app58044346.eu.auth0.com';
