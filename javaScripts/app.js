8c76d985f1967725568c843e48c59b4bb56ebad4
sinderpl <sinderpl@gmail.com>
2017-02-13 11:08:37 +0000

    Mongo database connected - basics

--- javaScripts/app.js
+++ javaScripts/app.js
@@ -1,10 +1,33 @@
-var express = require('express')
-var app = express()
+// Retrieve
+var MongoClient = require('mongodb').MongoClient;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+// Connect to the db
+  MongoClient.connect("mongodb://Test:Test@ds139187.mlab.com:39187/heroku_vh3f7203", function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+    var collection = db.collection("Users");
+    if(!err) {
+      console.log("Collection found");
+    }
+    else{
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+      console.log("Collection not found");
+      console.log(err);
+    }
+
+    collection.find().toArray(function(err, items) {});
+
+    var Data  = collection.findOne({"pin":"2345"}, function(err, item) {});
+    if(!err) {
+      console.log("Json Data: " + Data);
+    }
+    else{
+      console.log("Data retrieval failed");
+      console.log(err);
+    }
+  }
+  else {
+    console.log("Connection failed");
+    console.log(err);
+  }
+});
