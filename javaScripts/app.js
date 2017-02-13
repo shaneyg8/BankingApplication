36f9ff8b99873b081460660ae154a56938904c3c
sinderpl <sinderpl@gmail.com>
2017-02-13 17:13:26 +0000

    Objects created for JSON data

--- javaScripts/app.js
+++ javaScripts/app.js
@@ -1,50 +1,79 @@
 // Retrieve
 var MongoClient = require('mongodb').MongoClient
     , format = require('util').format;
+
+//Object definitions
+
+//Object for the current users personal data
+function userData(username, pin, deviceID, accountsID ){
+  //#TODO Add a unique user identifier
+  //Username of the current user
+  this.username = username;
+  //Pin of the currentUser #TODO might be redundant
+  this.pin = pin;
+  //Used to confirm if current device is registered
+  this.deviceID = deviceID;
+  //ID's of any accounts owned by the user
+  this.accountsID = accountsID;
+}
+
+//Object for the user account data
+function userAccount(ownerName, accountType, accID, accBalance, transactions){
+  //#TODO Add a unique user identifier
+  //Usename of the current user
+  this.ownerName = ownerName;
+  //The type of the account that this object represents
+  this.accountType = accountType;
+  //The ID of the account Object
+  this.accID = accID;
+  //Current balance of the account
+  this.accBalance = accBalance;
+  //An array of transaction objects made by this account
+  this.transactions = transactions;
+  for (s of transactions){
+    console.log(s);
+    console.log(s);
+  }
+}
 // Connect to the db
   MongoClient.connect("mongodb://Test:Test@ds139187.mlab.com:39187/heroku_vh3f7203", function(err, db) {
   if(!err) {
     console.log("We are connected");
-    var collection = db.collection("Users");
 
-    /**
-    //var collection = db.collection('test_insert');
 
-    collection.insert({a:2}, function(err, docs) {
-        collection.count(function(err, count) {
-          console.log(format("count = %s", count));
-        });
-      });
-    }else{
-      throw err;
-    }**/
-
+    //Find the desired collection
+    var collectionUsers = db.collection("Users");
+    var collectionAccounts = db.collection("Accounts");
     if(!err) {
       console.log("Collection found");
     }
     else{
-
       console.log("Collection not found");
       console.log(err);
     }
 
-    collection.find().toArray(function(err, items) {
+    //Find the desired username in the collection
+    collectionUsers.find({ "username": "Alan Niemiec"}).toArray(function(err, items) {
       console.dir(items);
     });
-    /**
-    var Data  =  collection.findOne({"_id" : "5819dceadcba0f45f191c5e5"}).toArray(function(err, item)) {});
 
-    if(!err) {
-      console.log("Json Data: " + Data);
-    }
-    else{
-      console.log("Data retrieval failed");
-      console.log(err);
-    }**/
+    //Find the desired username in the collection
+    collectionAccounts.find().toArray(function(err, items) {
+      items;
+      console.dir(items);
+    });
+
   }
+  //Else for if the connection fails. show errors
   else {
     console.log("Connection failed");
     console.log(err);
   }
+  //Close the database connection
   db.close();
 });
+
+//Find the data in collection
+function getData(collection, searchVariable, searchValue, returnType){
+
+}
