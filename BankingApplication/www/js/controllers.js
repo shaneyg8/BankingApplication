c979363f93825703efc5241861cb23302823c640
shaneyg8 <shanegleeson22@gmail.com>
2017-02-18 19:53:18 +0000

    Frontend Development - Transaction Pages

--- BankingApplication/www/js/controllers.js
+++ BankingApplication/www/js/controllers.js
@@ -1,6 +1,62 @@
 angular.module('app.controllers', [])
   
-.controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('aCCOUNTDETAILSCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('tRANSFERSCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('tRANSFERTOANOTHERACCOUNTCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('pAYEEDETAILSCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('pAYEMENTCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('tRANSACTIONSCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('mORECtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+      
+.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
--- BankingApplication/www/js/routes.js
+++ BankingApplication/www/js/routes.js
@@ -10,13 +10,83 @@ angular.module('app.routes', [])
     
   
 
-      .state('login', {
-    url: '/login',
-    templateUrl: 'templates/login.html',
-    controller: 'loginCtrl'
+      .state('tabsController.aCCOUNTDETAILS', {
+    url: '/page2',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/aCCOUNTDETAILS.html',
+        controller: 'aCCOUNTDETAILSCtrl'
+      }
+    }
   })
 
-$urlRouterProvider.otherwise('/login')
+  .state('tabsController.tRANSFERS', {
+    url: '/page6',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/tRANSFERS.html',
+        controller: 'tRANSFERSCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.tRANSFERTOANOTHERACCOUNT', {
+    url: '/page7',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/tRANSFERTOANOTHERACCOUNT.html',
+        controller: 'tRANSFERTOANOTHERACCOUNTCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.pAYEEDETAILS', {
+    url: '/page8',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/pAYEEDETAILS.html',
+        controller: 'pAYEEDETAILSCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.pAYEMENT', {
+    url: '/page9',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/pAYEMENT.html',
+        controller: 'pAYEMENTCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.tRANSACTIONS', {
+    url: '/page3',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/tRANSACTIONS.html',
+        controller: 'tRANSACTIONSCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.mORE', {
+    url: '/page4',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/mORE.html',
+        controller: 'mORECtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+$urlRouterProvider.otherwise('/page1/page2')
 
   
 
