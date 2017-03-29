10e62bf210560edf46a26d8e3e40e3b125778979
sinderpl <sinderpl@gmail.com>
2017-03-29 15:00:53 +0000

    login started

---
+++ www/js/controllers.js
@@ -0,0 +1,186 @@
+angular.module('app.controllers', [])
+
+
+.controller('aCCOUNTSCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+
+.controller('aCCOUNTDETAILSCtrl', ['$scope', '$stateParams', '$http',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams, $http) {
+
+  //Create a custom HTTP POST request and query the external API
+  $http({
+    //Type of request - used POST since it is more secure than GET
+    method: 'POST',
+    //The URL to which call will be made
+    url: 'https://mobilebanking.herokuapp.com/user',
+    //The origin of the requeset (Current host)
+    origin: 'http://localhost:8100',
+    //The type of data being sent
+    dataType: "JSON",
+    //The data sent with which to query
+    //This is not in JSON format so should be investigated for safety
+    //Might be posted through url - need to verify
+    data : "username=alanniemiec&pin=2345" ,
+    //The header for the call being made
+    headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+    }).then(function successCallback(response) {
+        //Function activated if data is succesfully returned
+        console.log('success');
+        console.log(response.data);
+
+        //Set the ionic Scope variables for this page based on
+        // the data to display
+        $scope.ownerName = response.data.username;
+        $scope.accountNumber = response.data.accounts[0].accid;
+        $scope.accountBalance = response.data.accounts[0].balance;
+
+        // this callback will be called asynchronously
+        // when the response is available
+      }, function errorCallback(response) {
+          console.log('failure');
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+      });
+
+      $http({
+
+        method: 'POST',
+
+        url: 'https://mobilebanking.herokuapp.com/account',
+
+        origin: 'http://localhost:8100',
+
+        dataType: "JSON",
+
+        data : "accid=654321" ,
+
+        headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+        }).then(function successCallback(response) {
+        //Function activated if data is succesfully returned
+        console.log('success');
+        console.log(response.data);
+
+        //Set the ionic Scope variables for this page based on
+        // the data to display
+        $scope.trans = response.data.transactions;
+
+
+        //$scope.accountBalance = response.data.accounts[0].balance;
+
+        // this callback will be called asynchronously
+        // when the response is available
+      }, function errorCallback(response) {
+          console.log('failure');
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+      });
+
+
+}])
+
+.controller('tRANSFERSCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+.controller('tRANSFERTOANOTHERACCOUNTCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+.controller('pAYEEDETAILSCtrl', ['$scope', '$stateParams', '$http', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams, $http) {
+
+$scope.formData = {};
+
+$scope.addPayee = function(){
+    $http.post('https://mobilebanking.herokuapp.com/user', $scope.formData)
+    .success(function(data){
+        $scope.formData = {};
+        $scope.payee = data;
+        console.log(data);
+
+    })
+    .error(function(data){
+        console.log('Error: ' + data);
+    });
+};
+
+
+}])
+
+.controller('pAYEMENTCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+.controller('tRANSACTIONSCtrl', ['$scope', '$stateParams','$http', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams, $http) {
+
+	 //Create a custom HTTP POST request and query the external API
+  $http({
+    //Type of request - used POST since it is more secure than GET
+    method: 'POST',
+    //The URL to which call will be made
+    url: 'https://mobilebanking.herokuapp.com/account',
+    //The origin of the requeset (Current host)
+    origin: 'http://localhost:8100',
+    //The type of data being sent
+    dataType: "JSON",
+
+    //The data sent with which to query
+    //This is not in JSON format so should be investigated for safety
+    //Might be posted through url - need to verify
+    data : "accid=654321",
+    //The header for the call being made
+    headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+    }).then(function successCallback(response) {
+        //Function activated if data is succesfully returned
+        console.log('success');
+        console.log(response.data);
+
+        //Set the ionic Scope variables for this page based on
+        // the data to display
+
+
+
+        // this callback will be called asynchronously
+        // when the response is available
+      }, function errorCallback(response) {
+          console.log('failure');
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+      });
+
+}])
+.controller('mORECtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
---
+++ www/js/routes.js
@@ -0,0 +1,113 @@
+angular.module('app.routes', ['ionicUIRouter'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+
+      /*
+    The IonicUIRouter.js UI-Router Modification is being used for this route.
+    To navigate to this route, do NOT use a URL. Instead use one of the following:
+      1) Using the ui-sref HTML attribute:
+        ui-sref='tabsController.aCCOUNTDETAILS'
+      2) Using $state.go programatically:
+        $state.go('tabsController.aCCOUNTDETAILS');
+    This allows your app to figure out which Tab to open this page in on the fly.
+    If you're setting a Tabs default page or modifying the .otherwise for your app and
+    must use a URL, use one of the following:
+      /page1/tab4/accountdetails
+      /page1/tab1/accountdetails
+  */
+  .state('tabsController.aCCOUNTDETAILS', {
+    url: '/accountdetails',
+    views: {
+      'tab4': {
+        templateUrl: 'templates/aCCOUNTDETAILS.html',
+        controller: 'aCCOUNTDETAILSCtrl'
+      },
+      'tab1': {
+        templateUrl: 'templates/aCCOUNTDETAILS.html',
+        controller: 'aCCOUNTDETAILSCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.aCCOUNTS', {
+    url: '/accounts',
+    views: {
+      'tab4': {
+        templateUrl: 'templates/aCCOUNTS.html',
+        controller: 'aCCOUNTSCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.tRANSFERS', {
+    url: '/transfers',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/tRANSFERS.html',
+        controller: 'tRANSFERSCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.tRANSFERTOANOTHERACCOUNT', {
+    url: '/transfertoanotheraccount',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/tRANSFERTOANOTHERACCOUNT.html',
+        controller: 'tRANSFERTOANOTHERACCOUNTCtrl'
+      }
+    }
+  })
+
+  .state('pAYEEDETAILS', {
+    url: '/payeedetails',
+    templateUrl: 'templates/pAYEEDETAILS.html',
+    controller: 'pAYEEDETAILSCtrl'
+  })
+
+  .state('tabsController.pAYMENT', {
+    url: '/payment',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/pAYMENT.html',
+        controller: 'pAYMENTCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.tRANSACTIONS', {
+    url: '/transactions',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/tRANSACTIONS.html',
+        controller: 'tRANSACTIONSCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.mORE', {
+    url: '/page4',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/mORE.html',
+        controller: 'mORECtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+$urlRouterProvider.otherwise('/page1/accounts')
+
+
+});
