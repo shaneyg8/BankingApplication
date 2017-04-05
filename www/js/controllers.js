8550c3133ecfdb2fe09550877c2c57b96caa643e
sinderpl <sinderpl@gmail.com>
2017-04-05 15:36:46 +0100

    Login widget now showing, now configuration

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,8 +9,7 @@ function ($scope, $stateParams) {
 
 }])
 
-
-.controller('LoginController', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('LoginController', ['$scope', '$stateParams',  'authService' ,// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams, authService) {
