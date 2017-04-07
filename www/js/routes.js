d59c7324d11c5bc0ae5b3d557f65b5c77b0cb82b
dstarr86 <starrdara@gmail.com>
2017-04-07 13:30:24 +0100

    getting rid of burger menu button

--- www/js/routes.js
+++ www/js/routes.js
@@ -10,7 +10,7 @@ angular.module('app.routes', ['ionicUIRouter'])
   .state('tabsController.aCCOUNTDETAILS', {
     url: '/accountdetails',
     views: {
-      'tab4': {
+      'tab1': {
         templateUrl: 'templates/aCCOUNTDETAILS.html',
         controller: 'aCCOUNTDETAILSCtrl'
       },
