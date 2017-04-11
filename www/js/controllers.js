f613ae3539edf8149489248d1acd04db64656c33
sinderpl <sinderpl@gmail.com>
2017-04-11 20:22:58 +0100

    Details added to Google map pin

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -247,6 +247,44 @@ function ($scope, $stateParams, $cordovaGeolocation) {
       position: latLng
   });
 
+
+  var contentString = '<div id="content">'+
+              '<div id="siteNotice">'+
+              '</div>'+
+              '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+              '<div id="bodyContent">'+
+              '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+              'sandstone rock formation in the southern part of the '+
+              'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+              'south west of the nearest large town, Alice Springs; 450&#160;km '+
+              '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+              'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+              'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+              'Aboriginal people of the area. It has many springs, waterholes, '+
+              'rock caves and ancient paintings. Uluru is listed as a World '+
+              'Heritage Site.</p>'+
+              '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+              'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+              '(last visited June 22, 2009).</p>'+
+              '</div>'+
+              '</div>';
+
+  var infoWindow = new google.maps.InfoWindow({
+      content: "<h1>GMIT Bank, Dublin Road</h2>"+
+                "<p>Opening Times: </p>"+
+                "<p>Monday:   9:00 - 17:00</p>"+
+                "<p>Tuesday:  9:00 - 17:00</p>"+
+                "<p>Wednsday: 9:00 - 17:00</p>"+
+                "<p>Thursday: 9:00 - 17:00</p>"+
+                "<p>Friday:   9:00 - 17:00</p>"+
+                "<p>Saturday: Closed</p>"+
+                "<p>Sunday:   Closed</p>"
+  });
+
+  google.maps.event.addListener(marker, 'click', function () {
+      infoWindow.open($scope.map, marker);
+  });
+
 });
 
   }, function(error){
